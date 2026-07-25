66222717bcbda877979fda1314ca72492d18bf91
Modder556 <eli.b.atkins@gmail.com>
2026-07-25 09:26:35 -0700

    Delete Main.java

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java — the one that started this");
-        for (int i = 1; i <= 5; i++) System.out.println(i + " squared is " + (i*i));
-    }
-}
